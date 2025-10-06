cc6b46fdc456c11969731afa3f746bc08ed7ced1
Ivannieto12 <ivannieto046@gmail.com>
2025-10-06 20:21:43 +0200

    Clase corregida

--- src/AppForSEII2526.API/Models/Alquiler.cs
+++ src/AppForSEII2526.API/Models/Alquiler.cs
@@ -4,36 +4,24 @@
 {
     public class Alquiler
     {
-        public Alquiler()
-        {
-        }
-        public Alquiler(string apellidoCliente, string correo, string direccionEnvio,DateTime fechaAlquiler,DateTime fechaFin,DateTime fechaInicio,string nombreCliente,string numeroTelefono,int periodo,double precioTotal,PaymentMethodTypes metodoPagos)
-        {
-        ApellidoCliente = apellidoCliente;
-        Correo = correo;
-        DireccionEnvio = direccionEnvio;
-        FechaAlquiler = fechaAlquiler;
-        FechaFin = fechaFin;
-        FechaInicio = fechaInicio;
-        NombreCliente = nombreCliente;
-        NumeroTelefono = numeroTelefono;
-        Periodo = periodo;
-        PrecioTotal = precioTotal;
-        MetodoPagos = metodoPagos;
+       
+        
 
-        }
+        
 
     public int Id { get; set; }
     public IList<AlquilarItem> AlquilarItems { get; set; }
-   
-   
 
+    [StringLength(50, ErrorMessage = "El correo no puede tener mas de 50 caracteres")] 
+    public string Correo { get; set; }
     public double PrecioTotal { get; set; }
 
         public DateTime FechaAlquiler { get; set; }
 
         public DateTime FechaFin { get; set; }
         public DateTime FechaInicio { get; set; }
+        [StringLength(9, ErrorMessage = "El numero de telefono no puede tener mas de 9 caracteres.")] 
+        public string NumeroTelefono { get; set; }
 
 
         [DataType(System.ComponentModel.DataAnnotations.DataType.MultilineText)]
@@ -41,16 +29,18 @@
         [Required(AllowEmptyStrings = false, ErrorMessage = "Introduce la direccion de envio")]
         public string DireccionEnvio { get; set; }
 
+        [StringLength(50, ErrorMessage = "El apellido no puede tener mas de 50 caracteres")] 
         public string ApellidoCliente { get; set; }
-
+        [StringLength(50, ErrorMessage = "El nombre no puede tener mas de 50 caracteres")]
         public string NombreCliente { get; set; }
         [Display(Name = "Payment Method")]
         public PaymentMethodTypes MetodoPagos { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
 
-        
 
-        
-    }
+
+
+}
 
     public enum MetodosPago
     {
