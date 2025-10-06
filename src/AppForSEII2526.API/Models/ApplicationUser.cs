fec857b530b174555e847d69ea01c27f6b50a89a
Jose Luis De Haro Córcoles <joseluis.haro@alu.uclm.es>
2025-10-06 21:00:20 +0200

    Crear ApplicationUser (#30)
    Co-authored-by: ivannnn12 <Ivan.Nieto@alu.uclm.es>

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -2,7 +2,29 @@
 
 namespace AppForSEII2526.API.Models;
 
-// Add profile data for application users by adding properties to the ApplicationUser class
-public class ApplicationUser : IdentityUser {
-  
+public class ApplicationUser : IdentityUser
+{
+    [Required]
+    [Display(Name = "Nombre")]
+    public string Nombre { get; set; }
+
+    [Required]
+    [Display(Name = "Apellidos")]
+    public string Apellidos { get; set; }
+
+    [Required]
+    [Display(Name = "Dirección de envío")]
+    public string DireccionEnvio { get; set; }
+
+    [Display(Name = "Rol de usuario")]
+    public RolUsuario Rol { get; set; }
+
+    [Display(Name = "Fecha de registro")]
+    public DateTime FechaRegistro { get; set; } = DateTime.Now;
+}
+
+public enum RolUsuario
+{
+    Administrador,
+    Cliente
 }
