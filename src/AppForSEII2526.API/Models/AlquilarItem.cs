60191deed3024cf43147d804abcfb6fb590e0c15
Ivannieto12 <ivannieto046@gmail.com>
2025-10-06 17:32:41 +0200

    Clase AlquilarItem terminada

--- src/AppForSEII2526.API/Models/AlquilarItem.cs
+++ src/AppForSEII2526.API/Models/AlquilarItem.cs
@@ -1,8 +1,26 @@
 ﻿using System;
 
-public class Class1
-{
-	public Class1()
-	{
-	}
-}
+
+    public class AlquilarItem
+    {
+        public AlquilarItem()
+        {
+        }
+
+        public AlquilarItem(int cantidad,double precio,int idAlquiler,int idHerramienta)
+        {
+        Cantidad = cantidad;
+        Precio = precio;
+        IdAlquiler = idAlquiler;
+        IdHerramienta=idHerramienta
+        }
+
+
+    public int Cantidad { get; set; }
+
+    public IdAlquiler { get; set; }
+
+    public int IdHerramienta { get; set; }
+    
+    public double Precio { get; set; }
+    }
