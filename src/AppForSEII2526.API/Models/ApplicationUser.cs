065622c303dd515a7ab4376cbc8177ed7c8853b4
Ivannieto12 <ivannieto046@gmail.com>
2025-10-06 18:06:33 +0200

    Clase corregida

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -4,4 +4,7 @@ namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
+    public string Nombre { get; set; }
+    public string Apellidos { get; set; }
+
 }
