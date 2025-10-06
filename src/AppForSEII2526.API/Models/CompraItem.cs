467b6d3fef9c4f5f1a961adea4b8902777f20172
MiguelLopez25 <Miguel.Lopez28@alu.uclm.es>
2025-10-06 20:14:40 +0200

    Clase CompraItem

---
+++ src/AppForSEII2526.API/Models/CompraItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+[PrimaryKey(nameof(IdHerramienta), nameof(IdCompra))]
+public class CompraItem
+{
+    [Range(1, int.MaxValue, ErrorMessage = "Cantidad mínima de compra es 1")]
+    public int Cantidad { get; set; }
+
+    [StringLength(150, ErrorMessage = "Descripción no puede ser mas largo que 150 caracteres.")]
+    public string? Descripcion { get; set; }
+
+    public Compra Compra { get; set; }
+    public int idCompra { get; set; }
+
+    Precision(7,2)
+    public float precio { get; set; }
+
+    public Herramienta Herramienta { get; set; }
+    public int idHerramienta { get; set; }
+}
