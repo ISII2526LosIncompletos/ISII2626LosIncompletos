caf1f782574b32f0204b428dfbd794b3775d6356
JOSÉ LUIS DE HARO CORCOLES <JoseLuis.Haro@alu.uclm.es>
2025-10-06 18:59:05 +0200

    Codigo de Clase Oferta

---
+++ src/AppForSEII2526.API/Models/Oferta.cs
@@ -0,0 +1,37 @@
+﻿namespace AppForSEII2526.API.Models
+
+{
+    public class Oferta
+    {
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime FechaInicio { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime FechaFinal { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime FechaOferta { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "El método de pago es obligatorio")]
+        [Display(Name = "Método de Pago")]
+        public MetodoPago MetodoPago { get; set; }
+
+        [Display(Name = "Oferta dirigida a")]
+        public TiposDirigidaOferta? TiposDirigidaOferta { get; set; }
+        
+        //public IList<OfertaItem> OfertaItems { get; set; }
+    }
+
+    public enum MetodoPago
+    {
+        PayPal,
+        Bizum,
+        Tarjeta
+    }
+
+    public enum TiposDirigidaOferta
+    {
+        Socios,
+        Clientes
+    }
+}
