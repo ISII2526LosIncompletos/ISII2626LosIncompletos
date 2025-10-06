c9636ee3cd84b82b2abaad3dd00e4beef9f0129c
Ivannieto12 <ivannieto046@gmail.com>
2025-10-06 20:32:26 +0200

    Corregido

--- src/AppForSEII2526.API/Models/AlquilarItem.cs
+++ src/AppForSEII2526.API/Models/AlquilarItem.cs
@@ -8,12 +8,11 @@ public class AlquilarItem
 
         
 
-    [PrimaryKey(nameof(HerramientaId),
-    nameof(IdAlquiler))]
+   
 
     public int Cantidad { get; set; }
 
-    public IdAlquiler { get; set; }
+    public int IdAlquiler { get; set; }
     public Alquiler Alquiler { get; set; }
     public Herramienta herramienta { get; set; }
     public int IdHerramienta { get; set; }
