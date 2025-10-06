819e09640d0f9398cb952d54180a0ef8f441e3ec
ivannnn12 <Ivan.Nieto@alu.uclm.es>
2025-10-06 20:29:34 +0200

    Rama alquiler (#22)
    * Clase alquiler hecha
    
    * Revert "Clase alquiler hecha"
    
    This reverts commit d96928d94332d12192e65e6ed2c44f6bdc37493d.
    
    * clase alquiler creada
    
    * Clase alquiler terminada
    
    * Clase corregida
    
    * Terminada
    
    * Clase corregida
    
    * Corregido
    
    ---------
    
    Co-authored-by: Ivannieto12 <ivannieto046@gmail.com>

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -4,4 +4,5 @@ namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
+  
 }
