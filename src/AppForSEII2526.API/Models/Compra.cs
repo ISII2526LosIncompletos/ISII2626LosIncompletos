0128c4ce1079006b3b45c834db07e6cfcb78e8e7
MiguelLopez25 <Miguel.Lopez28@alu.uclm.es>
2025-10-11 12:21:19 +0200

    Cambios Rama compra (#35)
    * Clase Compra
    
    * Actualizar Compra
    
    ---------
    
    Co-authored-by: Jose Luis De Haro Córcoles <joseluis.haro@alu.uclm.es>

--- src/AppForSEII2526.API/Models/Compra.cs
+++ src/AppForSEII2526.API/Models/Compra.cs
@@ -1,44 +1,39 @@
-﻿using System;
-
-public class Compra
-
+﻿namespace AppForSEII2526.API.Models
 {
-    public int Id { get; set; }
 
-    [StringLength(25, ErrorMessage = "El nombre no puede ser más de 25 caracteres, ni menos de 1.", MinimumLength = 1)]
-     public string NombreCliente { get; set; }
+    public class Compra
 
-    [StringLength(25, ErrorMessage = "El apellido no puede ser más de 25 caracteres, ni menos de 1.", MinimumLength = 1)]
-    public string ApellidoCliente { get; set; }
-
-    [DataType(DataType.Date)]
-    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
-    public DateTime fechaCompra { get; set; }
-
-    [Precision(5, 2)]
-    public decimal PrecioTotal { get; set; }
-
-    [DataType(DataType.PhoneNumber)]
-    public string? teléfono { get; set; }
-
-    [Display(Name = "Metodos de pago")]
-    public TiposMetodosPago metodoPago { get; set; }
-
-    [DataType(System.ComponentModel.DataAnnotations.DataType.MultilineText)]
-    [Required(AllowEmptyStrings = false, ErrorMessage = "Por favor, escribe la dirección de envío")]
-    public string direcciónEnvío { get; set; }
-
-    [DataType(DataType.Email)]
-    public string correoElectrónico { get; set; }
-
-    public List<CompraItem> CompraItems { get; set; }
-
-    public ApplicationUser ApplicationUser { get; set; }
-
-    public enum tiposMetodoPago
     {
-        TarjetaCredito,
-        PayPal,
-        Efectivo
+        [Key]
+        public int Id { get; set; }
+
+
+        [Required]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime fechaCompra { get; set; }
+
+        [Required]
+        [Precision(5, 2)]
+        public decimal PrecioTotal { get; set; }
+
+
+        [Required]
+        [Display(Name = "Metodos de pago")]
+        public tiposMetodoPago metodoPago { get; set; }
+
+        [DataType(System.ComponentModel.DataAnnotations.DataType.EmailAddress)]
+        public string correoElectrónico { get; set; }
+
+        public List<CompraItem> CompraItems { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public enum tiposMetodoPago
+        {
+            TarjetaCredito,
+            PayPal,
+            Efectivo
+        }
     }
 }
