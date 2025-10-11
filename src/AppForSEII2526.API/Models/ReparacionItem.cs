c71d57a95eb1220218a23618641e12f184916cc2
LuciaUCLM <lucia.cebrian2@alu.uclm.es>
2025-10-11 11:44:15 +0200

    Cambios rama ReparacionItem (#34)
    * Se ha creado la clase ReparacionItem.
    
    * Solución errores clase ReparacionItem.
    
    ---------
    
    Co-authored-by: MiguelLopez25 <Miguel.Lopez28@alu.uclm.es>

--- src/AppForSEII2526.API/Models/ReparacionItem.cs
+++ src/AppForSEII2526.API/Models/ReparacionItem.cs
@@ -1,18 +1,23 @@
-[PrimaryKey(nameof(IdHerramienta), nameof(IdReparacion))]
-public class ReparacionItem
+namespace AppForSEII2526.API.Models
 {
-    public Herramienta Herramienta { get; set; }
-    public int IdHerramienta { get; set; }
+    PrimaryKey(nameof(IdHerramienta), nameof(IdReparacion))]
+public class ReparacionItem
+    {
+        public Herramienta Herramienta { get; set; }
+        public int IdHerramienta { get; set; }
 
-    public Reparacion Reparacion { get; set; }
-    public int IdReparacion { get; set; }
+        public Reparacion Reparacion { get; set; }
+        public int IdReparacion { get; set; }
 
-    [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser superior a 1.")]
-    public int Cantidad { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser superior a 1.")]
+        public int Cantidad { get; set; }
 
-    [StringLength(100, ErrorMessage = "La descripción no puede superar los 100 caracteres.")]
-    public string? Descripcion { get; set; }
+        [StringLength(100, ErrorMessage = "La descripciÃ³n no puede superar los 100 caracteres.")]
+        public string? Descripcion { get; set; }
 
-    [Precision(10, 2)]
-    public float precio { get; set; }
+        [Required]
+        [Precision(10, 2)]
+        public float precio { get; set; }
+    }
 }
