b72dfbfe91680725c5d045718392e0d158d5ed9b
LuciaUCLM <lucia.cebrian2@alu.uclm.es>
2025-10-11 11:28:07 +0200

    Solucionados los errores de la clase Reparacion.

--- src/AppForSEII2526.API/Models/Reparacion.cs
+++ src/AppForSEII2526.API/Models/Reparacion.cs
@@ -1,39 +1,37 @@
-public class Reparacion
+namespace AppForSEII2526.API.Models
 {
-    [Key]
-    public int Id { get; set; }
-
-    [StringLength(20, ErrorMessage = "Nombre no puede tener más de 20 caracteres ni menos de 1", MinimumLength = 1)]
-    public string nombreCliente { get; set; }
-
-    [StringLength(20, ErrorMessage = "Apellido no puede tener más de 20 caracteres ni menos de 1", MinimumLength = 1)]
-    public string apellidoCliente { get; set; }
-
-    [DataType(DataType.Date)]
-    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
-    public DateTime fechaEntrega { get; set; }
-
-    [DataType(DataType.Date)]
-    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
-    public DateTime fechaRecogida { get; set; }
-
-    [DataType(DataType.PhoneNumber)]
-    public string? numTelefono { get; set; } // Puede ser nulo, es decir, opcional
-
-    [Precision(5, 2)]
-    public float precioTotal { get; set; }
-
-    [Display(Name = "Metodos de pago")]
-    public TiposMetodosPago metodoPago { get; set; }
-
-    public List<ReparacionItem> ReparacionItem { get; set; }
-
-    public ApplicationUser ApplicationUser { get; set; }
-
-    public enum TiposMetodosPago
+    public class Reparacion
     {
-        TarjetaCredito,
-        PayPal,
-        Cash
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime fechaEntrega { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime fechaRecogida { get; set; }
+
+        [Required]
+        [Precision(5, 2)]
+        public float precioTotal { get; set; }
+
+        [Required]
+        [Display(Name = "Metodos de pago")]
+        public TiposMetodosPago metodoPago { get; set; }
+
+        public List<ReparacionItem> ReparacionItem { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public enum TiposMetodosPago
+        {
+            TarjetaCredito,
+            PayPal,
+            Cash
+        }
     }
 }
