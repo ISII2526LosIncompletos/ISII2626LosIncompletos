bb4e5080199975f9c9a7a0692df0fa724a12bc65
JOSÉ LUIS DE HARO CORCOLES <JoseLuis.Haro@alu.uclm.es>
2025-10-11 12:53:43 +0200

    Correccion de errores

--- src/AppForSEII2526.API/Models/OfertaItem.cs
+++ src/AppForSEII2526.API/Models/OfertaItem.cs
@@ -3,15 +3,20 @@
     [PrimaryKey(nameof(IdOferta), nameof(IdHerramienta))]
     public class OfertaItem
     {
+        [Required]
         public Herramienta Herramienta { get; set; }
+        [Required]
         public int IdHerramienta { get; set; }
+        [Required]
         public Oferta Oferta { get; set; }
+        [Required]
         public int IdOferta { get; set; }
 
-
+        [Required]
         [Range(0, 100, ErrorMessage = "El porcentaje estara entre 0 y 100")]
         public int Porcentaje { get; set; }
 
+        [Required]
         [Range(0.05, float.MaxValue, ErrorMessage = "El precio minimo es 0.05")]
         [Precision(10, 2)]
         public decimal PrecioFinal { get; set; }
