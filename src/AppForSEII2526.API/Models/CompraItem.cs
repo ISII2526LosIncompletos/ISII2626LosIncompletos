6e3b8d6b1dcc43af8ed7fcbad1c164643a6db506
MiguelLopez25 <Miguel.Lopez28@alu.uclm.es>
2025-10-11 12:30:25 +0200

    Cambios Rama compra item (#36)
    * Clase CompraItem
    
    * Actualizado CompraItem
    
    ---------
    
    Co-authored-by: Jose Luis De Haro Córcoles <joseluis.haro@alu.uclm.es>

--- src/AppForSEII2526.API/Models/CompraItem.cs
+++ src/AppForSEII2526.API/Models/CompraItem.cs
@@ -1,19 +1,23 @@
-﻿using System;
-[PrimaryKey(nameof(IdHerramienta), nameof(IdCompra))]
-public class CompraItem
-{
-    [Range(1, int.MaxValue, ErrorMessage = "Cantidad mínima de compra es 1")]
-    public int Cantidad { get; set; }
+﻿namespace AppForSEII2526.API.Models {
+    [PrimaryKey(nameof(IdHerramienta), nameof(IdCompra))]
+    public class CompraItem
+    {   
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Cantidad mínima de compra es 1")]
+        public int Cantidad { get; set; }
 
-    [StringLength(150, ErrorMessage = "Descripción no puede ser mas largo que 150 caracteres.")]
-    public string? Descripcion { get; set; }
+        [Required]
+        [StringLength(150, ErrorMessage = "Descripción no puede ser mas largo que 150 caracteres.")]
+        public string Descripcion { get; set; }
 
-    public Compra Compra { get; set; }
-    public int idCompra { get; set; }
+        public Compra Compra { get; set; }
+        public int idCompra { get; set; }
 
-    Precision(7,2)
-    public float precio { get; set; }
+        [Required]
+        [Precision(7,2)]
+        public float precio { get; set; }
 
-    public Herramienta Herramienta { get; set; }
-    public int idHerramienta { get; set; }
+        public Herramienta Herramienta { get; set; }
+        public int idHerramienta { get; set; }
+    }
 }
