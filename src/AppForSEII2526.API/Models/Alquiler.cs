78d24c43b974efa79ffe80792d606586c90f0064
Ivannieto12 <ivannieto046@gmail.com>
2025-10-12 14:23:03 +0200

    Corregido

--- src/AppForSEII2526.API/Models/Alquiler.cs
+++ src/AppForSEII2526.API/Models/Alquiler.cs
@@ -2,13 +2,18 @@
 {
     public class Alquiler
     {
+        [Key]
         public int Id { get; set; }
         public IList<AlquilarItem> AlquilarItems { get; set; }
 
+
+        [Required]
         [StringLength(50, ErrorMessage = "El correo no puede tener mas de 50 caracteres")]
         public string Correo { get; set; }
+        [Required]
         public double PrecioTotal { get; set; }
 
+        [Required]
         public DateTime FechaAlquiler { get; set; }
 
         public DateTime FechaFin { get; set; }
@@ -16,7 +21,7 @@
         [StringLength(9, ErrorMessage = "El numero de telefono no puede tener mas de 9 caracteres.")]
         public string NumeroTelefono { get; set; }
 
-
+      
         [DataType(System.ComponentModel.DataAnnotations.DataType.MultilineText)]
         [Display(Name = "Direccion de envio")]
         [Required(AllowEmptyStrings = false, ErrorMessage = "Introduce la direccion de envio")]
@@ -26,6 +31,7 @@
         public string ApellidoCliente { get; set; }
         [StringLength(50, ErrorMessage = "El nombre no puede tener mas de 50 caracteres")]
         public string NombreCliente { get; set; }
+        [Required]
         [Display(Name = "Payment Method")]
         public PaymentMethodTypes MetodoPagos { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
