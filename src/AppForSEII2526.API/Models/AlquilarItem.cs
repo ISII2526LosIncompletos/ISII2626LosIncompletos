1b5b359f7b4b41904fa379cd2e5d356cbe2a28e7
Ivannieto12 <ivannieto046@gmail.com>
2025-10-12 12:40:42 +0200

    Clase corregida

--- src/AppForSEII2526.API/Models/AlquilarItem.cs
+++ src/AppForSEII2526.API/Models/AlquilarItem.cs
@@ -1,21 +1,19 @@
-﻿using System;
+﻿namespace AppForSEII2526.API.Models
+{
+    [PrimaryKey(nameof(HerramientaId),
+    nameof(IdAlquiler))]
 
-[PrimaryKey(nameof(HerramientaId),
-nameof(IdAlquiler))]
-public class AlquilarItem
+    public class AlquilarItem
     {
-        
 
-        
+        public int Cantidad { get; set; }
 
-   
+        public int IdAlquiler { get; set; }
+        public Alquiler Alquiler { get; set; }
+        public Herramienta herramienta { get; set; }
+        public int IdHerramienta { get; set; }
 
-    public int Cantidad { get; set; }
-
-    public int IdAlquiler { get; set; }
-    public Alquiler Alquiler { get; set; }
-    public Herramienta herramienta { get; set; }
-    public int IdHerramienta { get; set; }
+        public double Precio { get; set; }
     
-    public double Precio { get; set; }
-    }
+}
+}
