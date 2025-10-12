5ab5ea5ae7f849ed202166aa26ee987368ae1f71
ivannnn12 <Ivan.Nieto@alu.uclm.es>
2025-10-12 17:46:38 +0200

    Rama herramienta (#43)
    * Clase herramienta creada
    
    * Clase herramienta terminada
    
    * Terminada
    
    * corregido
    
    * Clase corregida
    
    * Corregido
    
    ---------
    
    Co-authored-by: Ivannieto12 <ivannieto046@gmail.com>
    Co-authored-by: LuciaUCLM <lucia.cebrian2@alu.uclm.es>

--- src/AppForSEII2526.API/Models/Herramienta.cs
+++ src/AppForSEII2526.API/Models/Herramienta.cs
@@ -1,31 +1,32 @@
-﻿using System;
-
-
-   
+namespace AppForSEII2526.API.Models
+{
     public class Herramienta
     {
-       
-
-        
-
-    public int Id { get; set; }
-   
+        [Key]
+        public int Id { get; set; }
 
 
-    [StringLength(50, ErrorMessage = "El material no puede tener mas de 30 caracteres")]
+        [Required]
+        [StringLength(50, ErrorMessage = "El material no puede tener mas de 30 caracteres")]
         public string Material { get; set; }
 
+        [Required]
         [DataType(System.ComponentModel.DataAnnotations.DataType.Currency)]
         [Range(0.5, float.MaxValue, ErrorMessage = "El precioMinimo es 1 ")]
         [Display(Name = "Precio")]
         [Precision(10, 2)]
         public decimal Precio { get; set; }
 
-
+        [Required]
         [Display(Name = "Nombre")]
         [Range(0, int.MaxValue, ErrorMessage = "Error nombre")]
         public int Nombre { get; set; }
 
+        [Display(Name = "TiempoReparacion")]
+        [Range(0, int.MaxValue, ErrorMessage = "Error tiempo reparacion ")]
+        public int tiempoReparacion { get; set; }
+
+        [Required]
         [StringLength(50, ErrorMessage = "El fabricante no puede tener mas de 30 caracteres")]
         public string Fabricante { get; set; }
 
@@ -33,5 +34,5 @@
         public IList<CompraItem> CompraItems { get; set; }
         public IList<OfertaItem> OfertaItems { get; set; }
         public IList<ItemReparacion> ItemsReparacion { get; set; }
-
+    }
 }
