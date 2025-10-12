0bc9d348caec2709568f30ef287e41e0b763a08f
ivannnn12 <Ivan.Nieto@alu.uclm.es>
2025-10-12 20:08:12 +0200

    Rama alquiler (#42)
    * Clase alquiler hecha
    
    * Revert "Clase alquiler hecha"
    
    This reverts commit d96928d94332d12192e65e6ed2c44f6bdc37493d.
    
    * clase alquiler creada
    
    * Clase alquiler terminada
    
    * Clase corregida
    
    * Terminada
    
    * Clase corregida
    
    * Corregido
    
    * Clase corregida
    
    * Corregido
    
    ---------
    
    Co-authored-by: Ivannieto12 <ivannieto046@gmail.com>
    Co-authored-by: LuciaUCLM <lucia.cebrian2@alu.uclm.es>

--- src/AppForSEII2526.API/Models/Alquiler.cs
+++ src/AppForSEII2526.API/Models/Alquiler.cs
@@ -1,46 +1,24 @@
-﻿using System;
-
-
+namespace AppForSEII2526.API.Models
 {
     public class Alquiler
     {
-       
-        
+        [Key]
+        public int Id { get; set; }
+        public IList<AlquilarItem> AlquilarItems { get; set; }
 
-        
+        [Required]
+        public double precioTotal { get; set; }
 
-    public int Id { get; set; }
-    public IList<AlquilarItem> AlquilarItems { get; set; }
-
-    [StringLength(50, ErrorMessage = "El correo no puede tener mas de 50 caracteres")] 
-    public string Correo { get; set; }
-    public double PrecioTotal { get; set; }
-
+        [Required]
         public DateTime FechaAlquiler { get; set; }
 
         public DateTime FechaFin { get; set; }
         public DateTime FechaInicio { get; set; }
-        [StringLength(9, ErrorMessage = "El numero de telefono no puede tener mas de 9 caracteres.")] 
-        public string NumeroTelefono { get; set; }
 
-
-        [DataType(System.ComponentModel.DataAnnotations.DataType.MultilineText)]
-        [Display(Name = "Direccion de envio")]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Introduce la direccion de envio")]
-        public string DireccionEnvio { get; set; }
-
-        [StringLength(50, ErrorMessage = "El apellido no puede tener mas de 50 caracteres")] 
-        public string ApellidoCliente { get; set; }
-        [StringLength(50, ErrorMessage = "El nombre no puede tener mas de 50 caracteres")]
-        public string NombreCliente { get; set; }
+        [Required]
         [Display(Name = "Payment Method")]
         public PaymentMethodTypes MetodoPagos { get; set; }
         public ApplicationUser ApplicationUser { get; set; }
-
-
-
-
-}
 
     public enum MetodosPago
     {
