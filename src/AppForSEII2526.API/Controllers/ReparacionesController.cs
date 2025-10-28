baf4ca0f7b567c51269b5b68f651d06a73c2b2dc
LuciaUCLM <lucia.cebrian2@alu.uclm.es>
2025-10-28 21:20:03 +0100

    Creación del método get details (#79)
    Co-authored-by: MiguelLopez25 <Miguel.Lopez28@alu.uclm.es>

--- src/AppForSEII2526.API/Controllers/ReparacionesController.cs
+++ src/AppForSEII2526.API/Controllers/ReparacionesController.cs
@@ -1,5 +1,6 @@
 ﻿using AppForSEII2526.API.DTOs.HerramientaDTOs;
 using AppForSEII2526.API.DTOs.ReparacionDTOs;
+using System.Linq;
 
 namespace AppForSEII2526.API.Controllers
 {
@@ -10,6 +11,40 @@ namespace AppForSEII2526.API.Controllers
         private readonly ApplicationDbContext _context;
         private readonly ILogger<HerramientasController> _logger;
 
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(ReparacionDetalleDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetReparacion(int id)
+        {
+            if (_context.Reparaciones == null)
+            {
+                _logger.LogError("Error: La tabla Reparaciones no existe");
+                return NotFound();
+            }
+
+            var rental = await _context.Reparaciones
+             .Where(r => r.Id == id)
+                 .Include(r => r.ReparacionItem)
+                    .ThenInclude(ri => ri.Herramienta)
+             .Select(r => new ReparacionDetalleDTO(r.ApplicationUser.Nombre,
+                    r.ApplicationUser.Apellidos, r.FechaEntrega, r.FechaRecogida, r.PrecioTotal,
+                    r.ReparacionItem
+                        .Select(ri => new ReparacionItemDTO(ri.Herramienta.Id,
+                                ri.Herramienta.Nombre, ri.Precio, ri.Cantidad,
+                                ri.Descripcion))
+                        .ToList<ReparacionItemDTO>()))
+             .FirstOrDefaultAsync();
+
+            if (rental == null)
+            {
+                _logger.LogError($"Error: Reparación con ID {id} no existe");
+                return NotFound();
+            }
+
+            return Ok(rental);
+        }
 
 
         [HttpPost]
