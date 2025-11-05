42a8c828fe4c3d92da6bbe3744282b3425380db2
MiguelLopez25 <Miguel.Lopez28@alu.uclm.es>
2025-11-05 12:23:12 +0100

    Creo que he arreglado el metodo get del detailComprar

--- src/AppForSEII2526.API/Controllers/ComprarController.cs
+++ src/AppForSEII2526.API/Controllers/ComprarController.cs
@@ -1,6 +1,51 @@
-﻿namespace AppForSEII2526.API.Controllers
+﻿using AppForSEII2526.API.DTOs.CompraDTOs;
+using AppForSEII2526.API.DTOs.HerramientaDTOs;
+using AppForSEII2526.API.DTOs.ReparacionDTOs;
+
+namespace AppForSEII2526.API.Controllers
 {
-    public class ComprarController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComprarController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<ComprarController> _logger;
+        public ComprarController(ApplicationDbContext context, ILogger<ComprarController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(CompraDetalleDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetCompras(int id)
+        {
+            if (_context.Compras == null)
+            {
+                _logger.LogError("Error: Tabla Compras no existe");
+                return NotFound();
+            }
+            var compra = await _context.Compras
+           .Where(r => r.Id == id)
+               .Include(r => r.CompraItems)
+                  .ThenInclude(ri => ri.Herramienta)
+           .Select(r => new CompraDetalleDTO(r.ApplicationUser.Nombre,
+                  r.ApplicationUser.Apellidos, r.ApplicationUser.DireccionEnvio, r.PrecioTotal,r.FechaCompra, r.CompraItems
+                      .Select(ri => new CompraItem(ri.Herramienta, ri.Herramienta.Id,
+                              ri.Compra, ri.Compra.Id, ri.Cantidad,
+                              ri.Descripcion, ri.Precio))
+                      .ToList<CompraItem>()))
+           .FirstOrDefaultAsync();
+
+            if (compra == null)
+            {
+                _logger.LogError($"Error: Rental with id {id} does not exist");
+                return NotFound();
+            }
+
+
+            return Ok(compra);
+        }
     }
 }
