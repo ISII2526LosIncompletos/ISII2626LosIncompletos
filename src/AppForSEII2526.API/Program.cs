9036c56c3420efe6d821e544484ac65dc4aa64b2
MiguelLopez25 <Miguel.Lopez28@alu.uclm.es>
2025-11-29 19:31:16 +0100

    Arreglo de development

--- src/AppForSEII2526.API/Program.cs
+++ src/AppForSEII2526.API/Program.cs
@@ -98,18 +98,18 @@ using (var scope = app.Services.CreateScope()) {
 }
 
 // Configure the HTTP request pipeline.
-// ... después de construir 'app' y antes de app.UseSwagger():
-//var enableSwagger = builder.Configuration.GetValue<bool>("EnableSwagger", false);
-//if (app.Environment.IsDevelopment() || enableSwagger)
-//{
+//... después de construir 'app' y antes de app.UseSwagger():
+var enableSwagger = builder.Configuration.GetValue<bool>("EnableSwagger", true);
+if (app.Environment.IsDevelopment() || enableSwagger)
+{
     var log = app.Services.GetRequiredService<ILogger<Program>>();
-    log.LogInformation("Habilitando Swagger: Environment={env}, EnableSwagger={enable}", app.Environment.EnvironmentName);
+    log.LogInformation("Habilitando Swagger: Environment={env}, EnableSwagger={enable}", app.Environment.EnvironmentName, enableSwagger);
     app.UseSwagger();
     app.UseSwaggerUI(c => {
         //this facilitates to generate unique ids for the operations
         c.DisplayOperationId();
     });
-//}
+}
 
 app.UseHttpsRedirection();
 
