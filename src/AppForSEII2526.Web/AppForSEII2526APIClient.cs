ade3794246d047f718a362c08f1d80203d41947a
MiguelLopez25 <Miguel.Lopez28@alu.uclm.es>
2025-11-30 14:20:12 +0100

    Conexion configurada con la API

---
+++ src/AppForSEII2526.Web/AppForSEII2526APIClient.cs
@@ -0,0 +1,12 @@
+﻿
+internal class AppForSEII2526APIClient
+{
+    private string? uRI2API;
+    private HttpClient httpClient;
+
+    public AppForSEII2526APIClient(string? uRI2API, HttpClient httpClient)
+    {
+        this.uRI2API = uRI2API;
+        this.httpClient = httpClient;
+    }
+}
--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.EntityFrameworkCore;
 using AppForSEII2526.Web.Components;
 using AppForSEII2526.Web.Components.Account;
 using AppForSEII2526.Web.Data;
+using AppForSEII2526.Web.API;
+using AppForSEII2526.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,6 +36,12 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
     .AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+//this variable contains the url where the API has been deployed
+string? URI2API = builder.Configuration.GetValue(typeof(string), "AppForSEII2526_API") as string;
+
+//We creaete the service for accessing the API from where .WEB project
+builder.Services.AddScoped<AppForSEII2526APIClient>(sp => new AppForSEII2526APIClient(URI2API, new HttpClient()));
 
 var app = builder.Build();
 
