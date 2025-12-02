c29b3b35e2e6074e0b43605c8780d88a55f7d5cc
MiguelLopez25 <Miguel.Lopez28@alu.uclm.es>
2025-12-02 10:44:37 +0100

    Actualización de swagger en program.cs
    He modificado el swagger, para que se puede acceder a la api por azure con una variable a true

--- src/AppForSEII2526.API/Program.cs
+++ src/AppForSEII2526.API/Program.cs
@@ -97,14 +97,18 @@ using (var scope = app.Services.CreateScope()) {
     }
 }
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment()) {
+// Configure the HTTP request pipeline. //... después de construir 'app' y antes de app.UseSwagger():
+var enableSwagger = builder.Configuration.GetValue<bool>("EnableSwagger", true);
+//if (app.Environment.IsDevelopment() || enableSwagger)
+//{
+    var log = app.Services.GetRequiredService<ILogger<Program>>();
+    log.LogInformation("Habilitando Swagger: Environment={env}, EnableSwagger={enable}", app.Environment.EnvironmentName, enableSwagger);
     app.UseSwagger();
     app.UseSwaggerUI(c => {
         //this facilitates to generate unique ids for the operations
         c.DisplayOperationId();
     });
-}
+//
 
 app.UseHttpsRedirection();
 
