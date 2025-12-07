a8fa335df596b16c848d6c949714e83261c0fe4a
LuciaUCLM <lucia.cebrian2@alu.uclm.es>
2025-12-07 14:07:08 +0100

    Interfaz UI select reparar (#126)
    * Cambios archivo AppForSEII2526Web.csproj
    
    * RepararStateContainer creado para mantener la información de las herramientas seleccionadas
    
    * Añadimos el servicio a Program.cs para poder ser usado por los componentes blazor
    
    * Crear componente
    
    * Establecer la ruta para el componente blazor
    
    * Distribuir los componentes
    
    * Usar la API para obtener herramientas
    
    * Filtrar las herramientas para cargarlas
    
    * Incluir RepararStateContainer
    
    * Permitir seleccionar herramientas para repararlas
    
    * Comprobar posibles errores
    
    * Desarrollar carrito de reparaciones (y cambio de algunos nombres)
    
    * Solución problemas por no tener swagger1.json
    
    * Comprobar que sólo los usuarios autorizados pueden utilizar la aplicación

---
+++ src/AppForSEII2526.Web/RepararStateContainer.cs
@@ -0,0 +1,70 @@
+﻿using AppForSEII2526.Web.API;
+
+namespace AppForSEII2526.Web
+{
+    public class RepararStateContainer
+    {
+
+        //Creamos una instancia de Reparacion cuando se crea una instancia de RepararStateContainer
+        public ReparacionCreacionDTO Reparacion { get; private set; } = new ReparacionCreacionDTO()
+        {
+            ItemsReparacion = new List<ReparacionItemDTO>()
+        };
+
+        //Calculamos el precio total de la reparacion seleccionada
+        public decimal PrecioTotal
+        {
+            get
+            {
+                int recogida = (Reparacion.ItemsReparacion.Sum(r => r.TiempoReparacion));
+                int numDias = (Reparacion.FechaEntrega.AddDays(recogida) - Reparacion.FechaEntrega).Days;
+                return Convert.ToDecimal(Reparacion.ItemsReparacion.Sum(ri => ri.PrecioReparacion * numDias));
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+
+        public void AddHerramientaParaReparar(HerramientasDTO herramienta)
+        {
+            //Antes de añadir una herramienta, verificamos si ya está añadida.
+            if (!Reparacion.ItemsReparacion.Any(ri => ri.HerramientaID == herramienta.HerramientaID))
+                //La añadimos si no está ya en la lista
+                Reparacion.ItemsReparacion.Add(new ReparacionItemDTO()
+                    {
+                        HerramientaID = herramienta.HerramientaID,
+                        Nombre = herramienta.Nombre,
+                        PrecioReparacion = herramienta.Precio,
+                        TiempoReparacion = herramienta.TiempoReparacion, 
+                        Cantidad = herramienta.Cantidad,
+                        Descripcion = herramienta.Descripcion
+                    }
+                );
+        }
+
+        //Para borrar una herramienta de la lista de herramientas seleccionadas
+        public void RemoveReparacionItem(ReparacionItemDTO item)
+        {
+            Reparacion.ItemsReparacion.Remove(item);
+        }
+
+        //Eliminamos todas las herramientas de la lista
+        public void ClearRentingCart()
+        {
+            Reparacion.ItemsReparacion.Clear();
+        }
+
+        //Cuando finalizamos el proceso de reparación, creamos una Reparación
+        public void ReparacionProcessed()
+        {
+            //Hemos finalizado el proceso de reparación, creamos un nuevo objeto sin datos
+            Reparacion = new ReparacionCreacionDTO()
+            {
+                ItemsReparacion = new List<ReparacionItemDTO>()
+            };
+        }
+
+    }
+}
