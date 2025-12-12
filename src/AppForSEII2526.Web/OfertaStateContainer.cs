360f99bf4815b89e8513344a31607a023ae9f9cc
JOSÉ LUIS DE HARO CORCOLES <JoseLuis.Haro@alu.uclm.es>
2025-12-12 16:42:38 +0100

    Añador el OfertasStateContainer

---
+++ src/AppForSEII2526.Web/OfertaStateContainer.cs
@@ -0,0 +1,57 @@
+﻿using AppForSEII2526.Web.API;
+
+namespace AppForSEII2526.Web
+{
+    public class OfertaStateContainer
+    {
+        public OfertaForCreationDTO Oferta { get; private set; } = new OfertaForCreationDTO()
+        {
+            Items = new List<OfertaItemDTO>()
+        };
+
+        public float PrecioFinal
+        {
+            get
+            {
+                return (float)Oferta.Items.Sum(oi => oi.PrecioFinal);
+            }
+        }
+
+        public event Action? OnChange;
+
+        private void NotifyStateChanged() => OnChange?.Invoke();
+
+
+        public void AddHerramientaToOferta(HerramientasDTO herramienta)
+        {
+            if (!Oferta.Items.Any(oi => oi.HerramientaId == herramienta.HerramientaID))
+                Oferta.Items.Add(new OfertaItemDTO()
+                {
+                    HerramientaId = herramienta.HerramientaID,
+                    HerramientaNombre = herramienta.Nombre,
+                    HerramientaMaterial = herramienta.Material,
+                    PrecioOriginal = herramienta.Precio,
+                    FabricanteNombre = herramienta.Fabricante,
+                }
+            );
+        }
+
+        public void RemoveOfertaItemToOferta(OfertaItemDTO item)
+        {
+            Oferta.Items.Remove(item);
+        }
+
+        public void ClearOfertaCart()
+        {
+            Oferta.Items.Clear();
+        }
+
+        public void OfertaProcessed()
+        {
+            Oferta = new OfertaForCreationDTO()
+            {
+                Items = new List<OfertaItemDTO>()
+            };
+        }
+    }
+}
