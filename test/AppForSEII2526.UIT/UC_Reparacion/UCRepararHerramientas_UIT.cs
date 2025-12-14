d2cfcc1a50398c7d5dcbc2efcfcbcf96fe5375b9
LuciaUCLM <lucia.cebrian2@alu.uclm.es>
2025-12-14 19:54:30 +0100

    Añadimos el ID de la opción que queremos elegir. Realizamos el paso inicial de hacer clic en reparación

--- test/AppForSEII2526.UIT/UC_Reparacion/UCRepararHerramientas_UIT.cs
+++ test/AppForSEII2526.UIT/UC_Reparacion/UCRepararHerramientas_UIT.cs
@@ -9,10 +9,19 @@ namespace AppForSEII2526.UIT.UC_Reparacion
 {
     public class UCRepararHerramientas_UIT : UC_UIT
     {
+        private SelectHerramientasParaReparar_PO selectHerramientasParaReparar_PO;
+
         public UCRepararHerramientas_UIT(ITestOutputHelper output) : base(output)
         {
         }
 
+        private void InitialStepsParaRepararHerramientas()
+        {
+            //Esperamos a que la opción del menú sea visible
+            selectHerramientasParaReparar_PO.WaitForBeingVisible(By.Id("CreateReparacion"));
+            //Hacemos clic en la opción del menú para hacer una reparación
+            _driver.FindElement(By.Id("CreateReparacion")).Click();
+        }
 
     }
 }
