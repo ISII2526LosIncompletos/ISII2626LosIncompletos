49cc2e8577dda2b0b93245e7521bfaf21e3ebcb4
LuciaUCLM <lucia.cebrian2@alu.uclm.es>
2025-12-14 20:13:34 +0100

    Desarrollar el primer filtro: filtrar por nombre

--- test/AppForSEII2526.UIT/UC_Reparacion/UCRepararHerramientas_UIT.cs
+++ test/AppForSEII2526.UIT/UC_Reparacion/UCRepararHerramientas_UIT.cs
@@ -10,9 +10,16 @@ namespace AppForSEII2526.UIT.UC_Reparacion
     public class UCRepararHerramientas_UIT : UC_UIT
     {
         private SelectHerramientasParaReparar_PO selectHerramientasParaReparar_PO;
+        private const int herrId1 = 1;
+        private const string herrNombre1 = "Destornillador";
+        private const string herrMaterial1 = "Acero";
+        private const string herrFabricante1 = "Wurt";
+        private const string herrPrecio1 = "12.5";
+        private const string herrTiempoRep1 = "1";
 
         public UCRepararHerramientas_UIT(ITestOutputHelper output) : base(output)
         {
+            selectHerramientasParaReparar_PO = new SelectHerramientasParaReparar_PO(_driver, _output);
         }
 
         private void InitialStepsParaRepararHerramientas()
@@ -21,6 +28,22 @@ namespace AppForSEII2526.UIT.UC_Reparacion
             selectHerramientasParaReparar_PO.WaitForBeingVisible(By.Id("CreateReparacion"));
             //Hacemos clic en la opción del menú para hacer una reparación
             _driver.FindElement(By.Id("CreateReparacion")).Click();
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC2_1_2_3_AF0_filtering()
+        {
+            //Arrange
+            InitialStepsParaRepararHerramientas();
+            var expectedHerramientas = new List<string[]> { new string[] { herrNombre1, herrMaterial1, herrFabricante1, herrPrecio1, herrTiempoRep1 }, };
+
+            //Act
+            selectHerramientasParaReparar_PO.SearchHerramienta("Desto", "");
+
+            //Assert
+            Assert.True(selectHerramientasParaReparar_PO.CheckListaHerramientas(expectedHerramientas));
+
         }
 
     }
