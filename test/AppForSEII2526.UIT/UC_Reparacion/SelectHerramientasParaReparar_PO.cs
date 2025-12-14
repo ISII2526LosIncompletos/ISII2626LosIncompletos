cebd8dcdcfb04ca892d3a28a88cb7b3d18e37a13
LuciaUCLM <lucia.cebrian2@alu.uclm.es>
2025-12-14 19:38:01 +0100

    Cambios para que el filtro TiempoReparacion pueda dejarse en blanco

--- test/AppForSEII2526.UIT/UC_Reparacion/SelectHerramientasParaReparar_PO.cs
+++ test/AppForSEII2526.UIT/UC_Reparacion/SelectHerramientasParaReparar_PO.cs
@@ -8,8 +8,8 @@ namespace AppForSEII2526.UIT.UC_Reparacion
 {
     internal class SelectHerramientasParaReparar_PO : PageObject
     {
-        By inputNombre = By.Id("herrNombre");
-        By inputTiempoRep = By.Id("herrTiempoRep");
+        By inputNombre = By.Id("inputNombre");
+        By inputTiempoRep = By.Id("inputTiempoReparacion");
         By buttonSearchHerramienta = By.Id("SearchHerramienta");
         public SelectHerramientasParaReparar_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
@@ -19,8 +19,8 @@ namespace AppForSEII2526.UIT.UC_Reparacion
             //Espera a que se pueda hacer clic en el elemento web
             WaitForBeingClickable(inputNombre);
             _driver.FindElement(inputNombre).SendKeys(nombre);
-            WaitForBeingClickable(inputTiempoRep);
-            _driver.FindElement(inputTiempoRep).SendKeys(tiempoRep);
+            if (tiempoRep != null) 
+                _driver.FindElement(inputTiempoRep).SendKeys(tiempoRep);
             _driver.FindElement(buttonSearchHerramienta).Click();
 
 
