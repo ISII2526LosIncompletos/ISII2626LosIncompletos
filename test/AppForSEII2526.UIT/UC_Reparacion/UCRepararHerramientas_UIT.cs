338b0149ed582702cff1c0497f6228a005d08084
LuciaUCLM <lucia.cebrian2@alu.uclm.es>
2025-12-14 18:56:35 +0100

    Creación de la clase para implementar los casos de prueba de UIT

---
+++ test/AppForSEII2526.UIT/UC_Reparacion/UCRepararHerramientas_UIT.cs
@@ -0,0 +1,18 @@
+﻿using AppForMovies.UIT.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Reparacion
+{
+    public class UCRepararHerramientas_UIT : UC_UIT
+    {
+        public UCRepararHerramientas_UIT(ITestOutputHelper output) : base(output)
+        {
+        }
+
+
+    }
+}
