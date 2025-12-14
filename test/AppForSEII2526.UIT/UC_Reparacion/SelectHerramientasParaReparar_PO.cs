6aee8423a1cbb24af8d58f622b4c1e46eb4b5ca8
LuciaUCLM <lucia.cebrian2@alu.uclm.es>
2025-12-14 19:20:44 +0100

    Creación del PageObject. Añadir localizadores. Interactuar con la interfaz para buscar herramientas.

---
+++ test/AppForSEII2526.UIT/UC_Reparacion/SelectHerramientasParaReparar_PO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Reparacion
+{
+    internal class SelectHerramientasParaReparar_PO : PageObject
+    {
+        By inputNombre = By.Id("herrNombre");
+        By inputTiempoRep = By.Id("herrTiempoRep");
+        By buttonSearchHerramienta = By.Id("SearchHerramienta");
+        public SelectHerramientasParaReparar_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+        public void SearchHerramienta(string nombre, string tiempoRep)
+        {
+            //Espera a que se pueda hacer clic en el elemento web
+            WaitForBeingClickable(inputNombre);
+            _driver.FindElement(inputNombre).SendKeys(nombre);
+            WaitForBeingClickable(inputTiempoRep);
+            _driver.FindElement(inputTiempoRep).SendKeys(tiempoRep);
+            _driver.FindElement(buttonSearchHerramienta).Click();
+
+
+        }
+
+        
+    }
+}
