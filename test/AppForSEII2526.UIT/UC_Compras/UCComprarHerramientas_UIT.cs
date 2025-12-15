3b2e7dc427caf47e484f40f7b783c4635bd96459
MiguelLopez25 <Miguel.Lopez28@alu.uclm.es>
2025-12-15 21:30:23 +0100

    Creamos la clase para implementar los casos de prueba UIT

---
+++ test/AppForSEII2526.UIT/UC_Compras/UCComprarHerramientas_UIT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppForSEII2526.UIT.Shared;
+
+namespace AppForSEII2526.UIT.UC_Compras
+{
+    public class UCComprarHerramientas_UIT: UC_UIT
+    {
+        public UCComprarHerramientas_UIT(ITestOutputHelper output) : base(output)
+        {
+
+        }
+    }
+}
--- test/AppForSEII2526.UT/ComprarController_test/CreacionCompra_test.cs
+++ test/AppForSEII2526.UT/ComprarController_test/CreacionCompra_test.cs
@@ -99,7 +99,7 @@ namespace AppForSEII2526.UT.ComprarController_test
                 new object[] {compraSinDescripcion, "La descripcion no puede estar vacia"},
                 new object[] {compraCantidad, "La cantidad debe ser mayor que cero."},
                 new object[] {compraErronea, $"La herramienta '{compraErronea.CompraItems[0].Nombre}' no existe." },
-                new object[] {compraPagoEfectivo, "No aceptamos compras pagadas en efectivo" }
+                new object[] {compraPagoEfectivo, "Error! No aceptamos compras pagadas en efectivo" }
             };
 
             return allTest;
