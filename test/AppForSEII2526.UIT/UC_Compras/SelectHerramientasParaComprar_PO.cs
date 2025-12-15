4acb10bf2642bbc6a3021cb270393d974ced568a
MiguelLopez25 <Miguel.Lopez28@alu.uclm.es>
2025-12-15 21:39:57 +0100

    Creando Pageobject para interactuar con la interfaz para buscar herramientas. Localizadores añadidos

---
+++ test/AppForSEII2526.UIT/UC_Compras/SelectHerramientasParaComprar_PO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Compras
+{
+    public class SelectHerramientasParaComprar_PO: PageObject
+    {
+        By inputPrecio = By.Id("precioHerramienta");
+        By inputMaterial= By.Id("materialHerramienta");
+        By buttonSearchHerramienta = By.Id("buscarHerramientas");
+        public SelectHerramientasParaComprar_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        { 
+        }
+        public void BuscarHerramientas(string precio, string material)
+        {
+            //Espera a que el elemento sea clickable
+            WaitForBeingClickable(inputPrecio);
+            _driver.FindElement(inputPrecio).SendKeys(precio);
+            _driver.FindElement(buttonSearchHerramienta).Click();
+
+
+        }
+    }
+}
