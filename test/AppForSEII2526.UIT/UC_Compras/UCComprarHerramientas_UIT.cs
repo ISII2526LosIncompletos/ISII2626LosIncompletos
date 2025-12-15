637b110d0035f1b0a54f21bfd987c2994203144e
MiguelLopez25 <Miguel.Lopez28@alu.uclm.es>
2025-12-15 22:10:02 +0100

    Añadimos el id de la opción que queremos seleccionar. Realizamos el paso inicial de hacer click en compra

--- test/AppForSEII2526.UIT/UC_Compras/UCComprarHerramientas_UIT.cs
+++ test/AppForSEII2526.UIT/UC_Compras/UCComprarHerramientas_UIT.cs
@@ -9,9 +9,18 @@ namespace AppForSEII2526.UIT.UC_Compras
 {
     public class UCComprarHerramientas_UIT: UC_UIT
     {
+        private SelectHerramientasParaComprar_PO selectHerramientasParaComprar_PO;
         public UCComprarHerramientas_UIT(ITestOutputHelper output) : base(output)
         {
 
+        }
+        private void InitialStepsParaComprarHerramientas()
+        {
+
+            //Esperamos a que la opción del menú esté visible
+            selectHerramientasParaComprar_PO.WaitForBeingVisible(By.Id("CreateCompra"));
+            //Damos click en el menú
+            _driver.FindElement(By.Id("CreateCompra")).Click();
         }
     }
 }
