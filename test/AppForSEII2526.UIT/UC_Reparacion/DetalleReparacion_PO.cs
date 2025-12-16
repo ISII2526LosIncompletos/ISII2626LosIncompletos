eadbc863c21ff3aa57dbe414f46f6107f8fff0d1
LuciaUCLM <lucia.cebrian2@alu.uclm.es>
2025-12-16 19:47:19 +0100

    Creamos el PageObject necesario para las pruebas de la pantalla del detalle, el flujo básico

---
+++ test/AppForSEII2526.UIT/UC_Reparacion/DetalleReparacion_PO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Reparacion
+{
+    internal class DetalleReparacion_PO : PageObject
+    {
+        public DetalleReparacion_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+    }
+}
