c2328444ff6af07ff3cb9860d200c6277484f321
Jose Luis De Haro Córcoles <joseluis.haro@alu.uclm.es>
2025-12-16 18:51:50 +0100

    Subo las pruebas del Select (#156)

---
+++ test/AppForSEII2526.UIT/UC_Oferta/SelectHerramientasParaOfertar_PO.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using Xunit.Abstractions;
+using AppForSEII2526.UIT.Shared;
+
+namespace AppForSEII2526.UIT.CU_Ofertas
+{
+    public class SelectHerramientasParaOfertar_PO : PageObject
+    {
+
+        private By inputFabricante = By.Id("inputGenre");
+
+        private By inputPrecio = By.Id("inputTitle");
+
+        private By buttonSearchHerramientas = By.Id("buscarHerramientas");
+
+        private By tableOfHerramientasBy = By.Id("Tabla de herramientas");
+
+        private By errorShownBy = By.Id("ErrorsShown");
+
+        private By buttonCrearOfertaCarrito = By.Id("purchaseMovieButton");
+
+        public SelectHerramientasParaOfertar_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+
+        public void SearchHerramientas(string fabricante, string precio)
+        {
+            WaitForBeingClickable(inputPrecio);
+
+            var precioBox = _driver.FindElement(inputPrecio);
+            precioBox.Clear();
+            precioBox.SendKeys(precio);
+
+            var fabricanteBox = _driver.FindElement(inputFabricante);
+            fabricanteBox.Clear();
+            if (fabricante != "All" && !string.IsNullOrEmpty(fabricante))
+            {
+                fabricanteBox.SendKeys(fabricante);
+            }
+
+            _driver.FindElement(buttonSearchHerramientas).Click();
+        }
+
+        public void CrearOfertaCarrito()
+        {
+            WaitForBeingClickable(buttonCrearOfertaCarrito);
+            _driver.FindElement(buttonCrearOfertaCarrito).Click();
+        }
+
+        public bool CheckListOfHerramientas(List<string[]> expectedHerramientas)
+        {
+            var rows = _driver.FindElement(tableOfHerramientasBy).FindElements(By.TagName("tr"));
+
+            foreach (var expectedRowData in expectedHerramientas)
+            {
+                string textoEsperado = string.Join(" ", expectedRowData);
+
+                {
+                    textoEsperado = textoEsperado.Replace("12,50", "12,5");
+                }
+
+                if (textoEsperado.Contains("10,30"))
+                {
+                    textoEsperado = textoEsperado.Replace("10,30", "10,3");
+                }
+
+                bool filaEncontrada = rows.Any(row => row.Text.Contains(textoEsperado));
+
+                if (!filaEncontrada)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public bool CheckMessageError(string errorMessage)
+        {
+            WaitForBeingVisible(errorShownBy);
+            IWebElement actualErrorShown = _driver.FindElement(errorShownBy);
+            _output.WriteLine($"actual Message shown:{actualErrorShown.Text}");
+            return actualErrorShown.Text.Contains(errorMessage);
+        }
+
+        public void AddHerramientaToOfertaCart(string herramientaId)
+        {
+            By addBtn = By.Id("movieToRent_" + herramientaId);
+            WaitForBeingClickable(addBtn);
+            _driver.FindElement(addBtn).Click();
+        }
+
+        public void RemoveHerramientaFromOfertaCart(string herramientaId)
+        {
+            By removeBtn = By.Id("removeMovie_" + herramientaId);
+            WaitForBeingClickable(removeBtn);
+            _driver.FindElement(removeBtn).Click();
+        }
+
+        public bool OfertaNotAvailable()
+        {
+            try
+            {
+                var element = _driver.FindElement(buttonCrearOfertaCarrito);
+                return !element.Displayed;
+            }
+            catch (NoSuchElementException)
+            {
+                return true;
+            }
+        }
+    }
+}
---
+++ test/AppForSEII2526.UIT/UC_Oferta/UCOfertarHerramientas_UIT.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using OpenQA.Selenium;
+using Xunit.Abstractions;
+using AppForSEII2526.UIT.Shared;
+using AppForSEII2526.UIT.CU_Ofertas;
+
+namespace AppForSEII2526.UIT.CU_Ofertas
+{
+    public class UCOfertarHerramientas_UIT : UC_UIT
+    {
+        private SelectHerramientasParaOfertar_PO selectHerramientasParaOfertar_PO;
+
+        private const string herrId1 = "1";
+        private const string herrNombre1 = "Destornillador";
+        private const string herrMaterial1 = "Acero";
+        private const string herrFabricante1 = "Wurt";
+        private const string herrPrecio1 = "12,50";
+
+        private const string herrId2 = "2";
+        private const string herrNombre2 = "Llave Inglesa";
+        private const string herrMaterial2 = "Acero";
+        private const string herrFabricante2 = "Phillips";
+        private const string herrPrecio2 = "10,30";
+
+        public UCOfertarHerramientas_UIT(ITestOutputHelper output) : base(output)
+        {
+            selectHerramientasParaOfertar_PO = new SelectHerramientasParaOfertar_PO(_driver, _output);
+        }
+
+        private void InitialStepsParaOfertarHerramientas()
+        {
+            Initial_step_opening_the_web_page();
+
+            By menuLink = By.Id("SelectHerramientasParaOfertar");
+
+            selectHerramientasParaOfertar_PO.WaitForBeingVisible(menuLink);
+            Thread.Sleep(500);
+            _driver.FindElement(menuLink).Click();
+        }
+
+        [Theory]
+        [InlineData(herrNombre1, herrMaterial1, herrFabricante1, herrPrecio1, "Wurt", "")]
+        [InlineData(herrNombre1, herrMaterial1, herrFabricante1, herrPrecio1, "", "12,50")]
+        [InlineData(herrNombre2, herrMaterial2, herrFabricante2, herrPrecio2, "Phillips", "10,30")]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC3_FiltrarHerramientas_PorFabricanteYPrecio(string herrNombre, string herrMaterial, string herrFabricante,
+            string herrPrecio, string filtroFabricante, string filtroPrecio)
+        {
+            InitialStepsParaOfertarHerramientas();
+
+            var expectedHerramientas = new List<string[]>
+            {
+                new string[] { herrNombre, herrFabricante, herrMaterial, herrPrecio }
+            };
+
+            selectHerramientasParaOfertar_PO.SearchHerramientas(filtroFabricante, filtroPrecio);
+            Thread.Sleep(500);
+
+            Assert.True(selectHerramientasParaOfertar_PO.CheckListOfHerramientas(expectedHerramientas));
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC3_CarritoOfertas_BotonOcultoSiVacio()
+        {
+            InitialStepsParaOfertarHerramientas();
+            selectHerramientasParaOfertar_PO.SearchHerramientas("", "");
+            Thread.Sleep(500);
+
+            Assert.True(selectHerramientasParaOfertar_PO.OfertaNotAvailable());
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC3_CarritoOfertas_AñadirYBorrar()
+        {
+            InitialStepsParaOfertarHerramientas();
+            selectHerramientasParaOfertar_PO.SearchHerramientas("Wurt", "");
+            Thread.Sleep(500);
+
+            selectHerramientasParaOfertar_PO.AddHerramientaToOfertaCart(herrId1);
+            Thread.Sleep(500);
+
+            selectHerramientasParaOfertar_PO.RemoveHerramientaFromOfertaCart(herrId1);
+            Thread.Sleep(500);
+
+            Assert.True(selectHerramientasParaOfertar_PO.OfertaNotAvailable());
+        }
+    }
+}
