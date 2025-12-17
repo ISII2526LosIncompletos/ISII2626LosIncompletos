2ade8996c064523964a1e189a4f5bf0aca21fa05
MiguelLopez25 <Miguel.Lopez28@alu.uclm.es>
2025-12-17 11:44:09 +0100

    Issue161 desarrollo pruebas funcionales post detail compra (#164)
    * CU1_8 CarritoVacío
    
    * Caso de uso de modificar herramientas
    
    * Desarrollo de CU1_9 dato boligatorio no rellenado
    
    * Caso de uso de cantidad errónea
    
    * Cambios hechos

---
+++ test/AppForSEII2526.UIT/UC_Compras/DetallesCompra_PO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Compras
+{
+    internal class DetallesCompra_PO: PageObject
+    {
+        public DetallesCompra_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+        public bool CheckDetallesCompra(string nombre, string apellidos, string direccion, string precioTotal, DateTime fecha)
+        {
+            WaitForBeingClickable(By.Id("HerramientasCompradas"));
+            bool result = true;
+            var nombreYApellidos = nombre + " " + apellidos;
+            result = result && _driver.FindElement(By.Id("NombreApellidos")).Text.Contains(nombreYApellidos);
+            result = result && _driver.FindElement(By.Id("DireccionEnvio")).Text.Contains(direccion);
+            result = result && _driver.FindElement(By.Id("PrecioTotal")).Text.Contains(precioTotal);
+            result = result && _driver.FindElement(By.Id("FechaCompra")).Text.Contains(fecha.ToString("dd/MM/yyyy"));
+
+            return result;
+        }
+        public bool CheckListaHerramientasCompradas(List<string[]> expectedHerramientas)
+        {
+            return CheckBodyTable(expectedHerramientas, By.Id("HerramientasCompradas"));
+        }
+    }
+}
